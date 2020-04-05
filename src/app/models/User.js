3a93b7d572d3fbe08ecf028099255697510dd1bf
Fabiano de Matos Lopes <fabianomtlopes@yahoo.com.br>
2020-04-04 22:16:03 -0300

    modulo02-bootcamp

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,7 +1,6 @@
 import Sequelize, { Model } from 'sequelize';
 import bcrypt from 'bcryptjs';
 
-// Os campos abaixo nao e um espelho do banco
 class User extends Model {
   static init(sequelize) {
     super.init(
@@ -16,11 +15,13 @@ class User extends Model {
         sequelize,
       }
     );
+
     this.addHook('beforeSave', async user => {
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
     });
+
     return this;
   }
 
